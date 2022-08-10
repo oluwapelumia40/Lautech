8c0a4d8f24a623852d34eb07fbf3c1b206d87307
oluwapelumia40 <69686839+oluwapelumia40@users.noreply.github.com>
2022-08-10 01:42:16 +0100

    about code

--- src/About.jsx
+++ src/About.jsx
@@ -15,9 +15,15 @@ function About (){
                 <p> The philosophy of the LODLC is focused on how to open up access to high quality, international standard and appropriate education in a setting that is flexible, open, and civilized. To achieve this, only appropriate technology shall be deployed and a number of barriers to an engaging and satisfying learning experience will be minimized. </p>
                 <Link to="#!" className="read-more1"> READ MORE </Link>
                 </div>
-                <div className="about-col2"> 
-                <h1> ADMISSION INTO OPEN AND DISTANCE LEARNING (ODL) DEGREE PROGRAMMES FOR THE 2020/2021 ACADEMIC SESSION</h1>
-                </div>
+                    <div className="about-col2"> 
+                    <p className="about-col2-text1"> ADMISSION INTO OPEN AND DISTANCE LEARNING (ODL) DEGREE PROGRAMMES FOR THE 2020/2021 ACADEMIC SESSION</p>
+                    <p> Applications are hereby invited from qualified candidates into the ODL Bachelor’s Degree Programmes in the Open and Distance Learning Centre of Ladoke Akintola University of Technology, Ogbomoso for the 2020/2021 Academic Session. </p>
+                    <p className="about-col2-text1"> IMPORTANT NOTICE ON ADMISSION REQUIREMENTS</p>
+                    <p> Candidates who participated in the 2020/2021 UTME examination and scored 180 and above may also be considered for admission irrespective of their choice of institution. </p>
+                    <p className="about-col2-text1"> COMPUTER PROFICIENCY TRAINING </p>
+                    <p> Computer proficiency training and test will be conducted for all applicants.</p>
+                    <Link to="#" className="read-more2"> READ MORE </Link>
+                    </div>
             </div>
            <Footer/>
         </div>
